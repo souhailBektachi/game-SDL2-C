b25ad8430773c59f125fc780d0d489d4a1d108d4
souhail <souhalshiv@gmail.com>
2023-12-14 20:09:26 +0100

    adding the Fps to the csts file

--- src/Cfiles/csts.c
+++ src/Cfiles/csts.c
@@ -1,6 +1,7 @@
 #include "csts.h"
 
-
+const int FPS = 60;
+const int framdelay=1000/FPS;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_WIDTH = 480;
 const int MAX_LINE_SIZE=1024;
--- src/headers/csts.h
+++ src/headers/csts.h
@@ -5,6 +5,8 @@ extern const int SCREEN_HEIGHT;
 extern const int SCREEN_WIDTH;
 extern const int MAX_LINE_SIZE;
 extern const char WALLS_TYPES[8];
+extern const int FPS;
+extern const int framdelay;
 #define HASH_SIZE 1000
 
-#endif  
+#endif
