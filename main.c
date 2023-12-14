d50bf22c35be98da053722d920ec832c412afbc9
souhail <souhalshiv@gmail.com>
2023-12-14 23:50:22 +0100

    add delta time

--- main.c
+++ main.c
@@ -4,17 +4,30 @@
 int WinMain()
 {
 
-    // Uint32 framesStart;
-    // int frameTime;
-    // double deltaTime = 0.0f;
+    Uint32 framesStart;
+    int frameTime;
+    double deltaTime = 0.0f;
+    Uint32 lastFrameTime = SDL_GetTicks();
     game game;
     gameInit(&game, "game with no name");
 
     while (game.isRunning)
     {
-        // framesStart = SDL_GetTicks();
+        framesStart = SDL_GetTicks();
+
         handleEvents(&game);
+        update(&game, deltaTime);
         renderGame(&game);
+        frameTime = SDL_GetTicks() - framesStart;
+        Uint32 currentFrameTime = SDL_GetTicks();
+        deltaTime = (currentFrameTime - lastFrameTime) / 1000.0f;
+        lastFrameTime = currentFrameTime;
+
+        if (framdelay > frameTime)
+        {
+            printf("%f:%f\n", getDelta_time(&game.player.character), deltaTime);
+            SDL_Delay(framdelay - frameTime);
+        }
     }
     cleanGame(&game);
     SDL_Quit();
