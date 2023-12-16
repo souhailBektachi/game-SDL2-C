75013d4ab5e877e520117c95dd17f6c7d4e5696b
souhail <souhalshiv@gmail.com>
2023-12-17 00:34:49 +0100

    adding max deltatime

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ int WinMain()
     Uint32 framesStart;
     int frameTime;
     double deltaTime = 0.0f;
+    double maxDeltaTime = 0.05f;
     Uint32 lastFrameTime = SDL_GetTicks();
     game game;
     gameInit(&game, "game with no name");
 
     while (game.isRunning)
     {
+
         framesStart = SDL_GetTicks();
 
         handleEvents(&game);
@@ -21,8 +23,10 @@ int WinMain()
         frameTime = SDL_GetTicks() - framesStart;
         Uint32 currentFrameTime = SDL_GetTicks();
         deltaTime = (currentFrameTime - lastFrameTime) / 1000.0f;
+        if (deltaTime > maxDeltaTime)
+            deltaTime = maxDeltaTime;
         lastFrameTime = currentFrameTime;
-
+        // printf("deltaTime:%f\n", deltaTime);
         if (framdelay > frameTime)
         {
             SDL_Delay(framdelay - frameTime);
